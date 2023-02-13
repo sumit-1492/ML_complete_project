8f5264f760713f991dc812930666b1612826dc93
sumit-1492 <sumitlala99@gmail.com>
2023-02-13 07:56:49 +0530

    config and artifact file updated

---
+++ Insurance_Project/entity/config_entity.py
@@ -0,0 +1,37 @@
+import os, sys
+from Insurance_Project.exception import InsuraneceException
+from Insurance_Project.logger import logging
+from datetime import datetime
+
+FILE_NAME = "insurance.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME =  "test.csv"
+
+class TrainingPipelineconfig:
+    def __init__(self):
+        try:
+            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
+        except Exception as e:
+            raise InsuraneceException(e,sys)
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineconfig):
+        try:
+            self.database_name = "INSURANCE2"
+            self.collection_name = "INSURANCE_PROJECT2"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+        except Exception as e:
+            raise InsuraneceException(e,sys)
+
+#convertin data to dictionary for reading in terminal
+    def to_dict(self)->dict:
+        try:
+            return self.__dict__
+        except Exception as e:
+            raise InsuraneceException(e,sys)
+
+#class DataValidation:
+    #pass
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Insurance_Project.logger import logging
 from Insurance_Project.exception import InsuraneceException
 import os,sys
 from Insurance_Project.utils import get_collecttion_as_dataframe
+from Insurance_Project.entity.config_entity import DataIngestionConfig
+from Insurance_Project.entity import config_entity
 
 #def test_logger_and_exception():
     #try:
@@ -15,6 +17,9 @@ from Insurance_Project.utils import get_collecttion_as_dataframe
 
 if __name__== "__main__":
     try:
-        get_collecttion_as_dataframe(database_name = "INSURANCE2",collection_name = "INSURANCE_PROJECT2")
+        #get_collecttion_as_dataframe(database_name = "INSURANCE2",collection_name = "INSURANCE_PROJECT2")
+        training_pipeline_config = config_entity.TrainingPipelineconfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
